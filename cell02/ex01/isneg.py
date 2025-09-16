21f25fc96f2e66df1a5c220e87780873b48c4f84
pparapam-kmitl <pparapam@ppmk-mac.local>
2025-09-16 18:03:21 +0700

    isneg

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,11 @@
+user_num = input("Number: ")
+
+user_num_int = int(user_num)
+
+if user_num_int > 0 :
+    print("This number is positive.")
+elif user_num_int < 0 :
+    print("This number is negative.")
+else :
+    print("This number is both positive and negative.")
+    
