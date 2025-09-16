68d935f59e32f0f5b5c9c5c1f2b37d3cae60fa20
pparapam-kmitl <pparapam@ppmk-mac.local>
2025-09-16 16:55:38 +0700

    iszero

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,9 @@
+user_num = input("input number: ")
+
+user_num_int = int(user_num)
+
+if user_num_int == 0 :
+    print("This number is equal to zero.")
+else :
+    print("This number is different from zero.")
+    
